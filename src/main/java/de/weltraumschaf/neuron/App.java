9897753adc1135ad6b9aaa97a101ec1956646942
Weltraumschaf <ich@weltraumschaf.de>
2012-11-10 17:14:50 +0100

    Implement basicinput shell and fix pom and startscript.

--- src/main/java/de/weltraumschaf/neuron/App.java
+++ src/main/java/de/weltraumschaf/neuron/App.java
@@ -1,13 +1,26 @@
 package de.weltraumschaf.neuron;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) throws IOException {
+        final BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            final String input = inputReader.readLine();
+
+            if ("exit".equals(input)) {
+                System.out.println("bye bye!");
+                break;
+            }
+
+            System.out.println(input);
+        }
     }
 }
