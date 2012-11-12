3c3ed49cd916e97059f5cfcb59c1ece3a38de259
Weltraumschaf <ich@weltraumschaf.de>
2012-11-12 21:38:33 +0100

    Add documentation and implement node deletion.

--- src/main/java/de/weltraumschaf/neuron/App.java
+++ src/main/java/de/weltraumschaf/neuron/App.java
@@ -4,15 +4,30 @@ import de.weltraumschaf.commons.InvokableAdapter;
 import de.weltraumschaf.neuron.shell.InteractiveShell;
 
 /**
+ * The main neuron application.
  *
  * @author Sven Strittmatter <weltraumschaf@googlemail.com>
  */
 public class App extends InvokableAdapter {
 
-    public App(final String[] args) {
+    /**
+     * Hide constructor because it is not intended to create objects of this type
+     * outside of {@link App#main(de.weltraumschaf.commons.Invokable)}.
+     *
+     * @param args command line arguments
+     */
+    private App(final String[] args) {
         super(args);
     }
 
+    /**
+     * Main entry point for JVM.
+     *
+     * Catches all {@link Exception exception} and print their message to STDERR
+     * and exits with -1.
+     *
+     * @param args command line arguments
+     */
     public static void main(final String[] args) {
         final App app = new App(args);
 
@@ -20,11 +35,16 @@ public class App extends InvokableAdapter {
             InvokableAdapter.main(app);
             app.exit(0);
         } catch (Exception ex) {
-            app.getIoStreams().errorln(ex.getMessage());
+            app.getIoStreams().errorln("Exception: " + ex.getMessage());
             app.exit(-1);
         }
     }
 
+    /**
+     * Starts interactive shell.
+     *
+     * @throws Exception if, something bad happens at runtime.
+     */
     @Override
     public void execute() throws Exception {
         final InteractiveShell shell = new InteractiveShell(getIoStreams());
