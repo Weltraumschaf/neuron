273e17b8d051d6d67c6f59a3d4141f6d8d022e79
Weltraumschaf <ich@weltraumschaf.de>
2012-11-13 23:19:47 +0100

    Test all aspects of ShellCommand.

--- src/test/java/de/weltraumschaf/neuron/shell/ShellCommandTest.java
+++ src/test/java/de/weltraumschaf/neuron/shell/ShellCommandTest.java
@@ -11,11 +11,13 @@
  */
 package de.weltraumschaf.neuron.shell;
 
+import com.google.common.collect.Lists;
 import de.weltraumschaf.neuron.shell.ShellCommand.MainType;
 import de.weltraumschaf.neuron.shell.ShellCommand.SubType;
-import org.junit.Test;
-import static org.junit.Assert.*;
+import java.util.List;
 import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+import org.junit.Test;
 
 
 /**
@@ -23,6 +25,24 @@ import static org.hamcrest.CoreMatchers.*;
  * @author sxs
  */
 public class ShellCommandTest {
+
+    @Test
+    public void constructCommandWithoutSubCommand() {
+        final List<Token> args = Lists.newArrayList();
+        final ShellCommand sut = new ShellCommand(MainType.HELP, args);
+        assertThat(sut.getCommand(), is(MainType.HELP));
+        assertThat(sut.getSubCommand(), is(SubType.NONE));
+        assertThat(sut.getArguments(), is(args));
+    }
+
+    @Test
+    public void constructCommandWithSubCommand() {
+        final List<Token> args = Lists.newArrayList();
+        final ShellCommand sut = new ShellCommand(MainType.NODE, SubType.ADD, args);
+        assertThat(sut.getCommand(), is(MainType.NODE));
+        assertThat(sut.getSubCommand(), is(SubType.ADD));
+        assertThat(sut.getArguments(), is(args));
+    }
 
     @Test
     public void isCommand() {
@@ -34,8 +54,13 @@ public class ShellCommandTest {
         assertThat(ShellCommand.isCommand(Token.newToken("foo")), is(false));
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void determineBadCommand() {
+        ShellCommand.determineCommand(Token.newToken("foo"));
+    }
+
     @Test
-    public void getCommand() {
+    public void determineCommand() {
         assertThat(ShellCommand.determineCommand(Token.newToken("help")), is(MainType.HELP));
         assertThat(ShellCommand.determineCommand(Token.newToken("reset")), is(MainType.RESET));
         assertThat(ShellCommand.determineCommand(Token.newToken("exit")), is(MainType.EXIT));
@@ -53,13 +78,32 @@ public class ShellCommandTest {
         assertThat(ShellCommand.isCommand(Token.newToken("foo")), is(false));
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void determineBadSubCommand() {
+        ShellCommand.determineSubCommand(Token.newToken("foo"));
+    }
+
     @Test
-    public void getSubCommand() {
+    public void determineSubCommand() {
         assertThat(ShellCommand.determineSubCommand(Token.newToken("add")), is(SubType.ADD));
         assertThat(ShellCommand.determineSubCommand(Token.newToken("del")), is(SubType.DEL));
         assertThat(ShellCommand.determineSubCommand(Token.newToken("connect")), is(SubType.CONNECT));
         assertThat(ShellCommand.determineSubCommand(Token.newToken("list")), is(SubType.LIST));
         assertThat(ShellCommand.determineSubCommand(Token.newToken("info")), is(SubType.INFO));
+    }
+
+    @Test
+    public void testToString() {
+        final List<Token> args = Lists.newArrayList();
+        args.add(Token.newToken("foo"));
+        args.add(Token.newToken(23));
+
+        ShellCommand sut = new ShellCommand(MainType.HELP, args);
+        assertThat(sut.toString(), is("ShellCommand{mainCommand=help, subCommand=, arguments=[Token{type=LITERAL, "
+                + "value=foo}, Token{type=INTEGER, value=23}]}"));
+        sut = new ShellCommand(MainType.NODE, SubType.ADD, args);
+        assertThat(sut.toString(), is("ShellCommand{mainCommand=node, subCommand=add, arguments=[Token{type=LITERAL, "
+                + "value=foo}, Token{type=INTEGER, value=23}]}"));
     }
 
 }
