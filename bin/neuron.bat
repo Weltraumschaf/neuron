ed3ecdecabe37afce137ee4df1d8fff057da44f3
Weltraumschaf <ich@weltraumschaf.de>
2012-11-13 12:10:36 +0100

    Add windows batch file.

---
+++ bin/neuron.bat
@@ -0,0 +1,15 @@
+@REM
+@REM LICENSE
+@REM
+@REM "THE BEER-WARE LICENSE" (Revision 43):
+@REM "Sven Strittmatter" <weltraumschaf(at)googlemail(dot)com> wrote this file.
+@REM As long as you retain this notice you can do whatever you want with
+@REM this stuff. If we meet some day, and you think this stuff is worth it,
+@REM you can buy me a non alcohol-free beer in return.
+@REM
+@REM Copyright (C) 2012 "Sven Strittmatter" <weltraumschaf(at)googlemail(dot)com>
+@REM
+
+@echo off
+
+java -jar "..\target\neuron.jar"
