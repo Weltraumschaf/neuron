7396ab9917919fac8b7f5735bb172828df1f3c5c
Weltraumschaf <ich@weltraumschaf.de>
2012-11-14 18:30:03 +0100

    Beautify help message.

--- src/main/java/de/weltraumschaf/neuron/cmd/Help.java
+++ src/main/java/de/weltraumschaf/neuron/cmd/Help.java
@@ -29,17 +29,17 @@ class Help extends BaseCommand implements UseVersion {
      */
     private static final String HELP =
           "This is the Neuron Interactive shell version %s.%n%n"
-        + "Available commands:%n"
+        + "Available commands:%n%n"
         + "  help                           Show all available commands.%n"
         + "  reset                          Reset the whole environment.%n"
-        + "  exit                           Exit the interactive shell.%n"
+        + "  exit                           Exit the interactive shell.%n%n"
         + "  node add [AMOUNT]              Creates one new node or AMOUNT nodes.%n"
         + "  node del ID                    Disconnect and deletes the node with ID.%n"
         + "  node connect ID NEIGHBOR_ID    Connect two nodes.%n"
         + "  node list                      List all nodes.%n"
         + "  node info ID                   Print info of a node.%n"
         + "  node listen ID                 Listening for events of specified node.%n"
-        + "  node unlisten ID               Stop listening for events of specified node.%n"
+        + "  node unlisten ID               Stop listening for events of specified node.%n%n"
         + "  message FROM_ID TO_ID MESSAGE  Send a message from one node to other.%n%n";
     /**
      * Program version info.
