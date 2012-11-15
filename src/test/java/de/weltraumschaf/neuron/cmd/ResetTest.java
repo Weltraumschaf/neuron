134aa5899e8b5e87e98008fdd16ed2e052abd469
Weltraumschaf <ich@weltraumschaf.de>
2012-11-15 23:36:01 +0100

    Test reset command.

--- src/test/java/de/weltraumschaf/neuron/cmd/ResetTest.java
+++ src/test/java/de/weltraumschaf/neuron/cmd/ResetTest.java
@@ -11,17 +11,31 @@
  */
 package de.weltraumschaf.neuron.cmd;
 
+import com.google.common.collect.Lists;
+import de.weltraumschaf.commons.IO;
+import de.weltraumschaf.neuron.shell.Environment;
+import de.weltraumschaf.neuron.shell.EventHandler;
+import de.weltraumschaf.neuron.shell.Token;
+import java.util.List;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import org.junit.Ignore;
+import static org.mockito.Mockito.*;
 
 /**
  *
- * @author "Sven Strittmatter" <weltraumschaf@googlemail.com>
+ * @author Sven Strittmatter <weltraumschaf@googlemail.com>
  */
 public class ResetTest {
 
-    @Test @Ignore
+    private final IO io = mock(IO.class);
+    private final Environment env = spy(new Environment(new EventHandler(io)));
+    private final List<Token> args = Lists.newArrayList();
+    private final Reset sut = new Reset(env, io, args);
+
+    @Test
     public void execute() {
+        sut.execute();
+        verify(io, times(1)).println("Resetting...");
+        verify(env, times(1)).reset();
     }
+
 }
